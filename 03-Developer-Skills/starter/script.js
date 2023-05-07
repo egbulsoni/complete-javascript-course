51bfffbfb9caf57a79afd0c797aa798b6ef78ba4
Eduardo <egbulsoni@gmail.com>
2023-05-07 19:19:02 -0300

    done until session 5 (incl.)

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,110 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(calcAge(1991));
+
+console.log();
+// 58 and on
+*/
+/*
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+    console.log(curTemp);
+  }
+  return max - min;
+};
+*/
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  //   console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  //   console.log(max, min);
+  return max - min;
+};
+
+// calcTempAmplitude([3, 7, 4, 23]);
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+// max = 3
+// max = 7
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
+
+  console.table(measurement);
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  //   console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) Idnentify
+console.log(amplitudeBug);
+
+// Video 62
+
+const printForecast = function (arr) {
+  let message = '';
+  for (let i = 0; i < arr.length; i++) {
+    message += `${arr[i]}ºC in ${i + 1} days ...`;
+  }
+  console.log('...' + message);
+  return message;
+};
+
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
