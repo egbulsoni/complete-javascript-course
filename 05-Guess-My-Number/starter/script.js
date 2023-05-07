fb35f0467bfeb1ca3eb58c8d5c418bc09367e414
Eduardo <egbulsoni@gmail.com>
2023-05-07 20:46:22 -0300

    aula 75 em diante

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+/*
+document.querySelector('.message');
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🍕Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //   document.querySelector('.message').textContent =
+  // '🍕 Correct Number!');
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🍕 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
+  }
+});
+
+// aula 75 em diante
+// D:\2-Sites and resources\GetFreeCourses.Co-Udemy-The Complete JavaScript Course 2022 From Zero to Expert!\07 JavaScript in the Browser_ DOM and Events Fundamentals
